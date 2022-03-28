6c3da1e2164fa4247102f99be4f3fb199441d7ce
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-03-28 13:57:23 -0300

    getting started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import HomePage from './pages/homePage/index.jsx';
 import './App.css'
 
 function App() {
   return (
     <>
+      <HomePage />
     </>
   )
 }
---
+++ src/pages/homePage/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <>
+            <div>
+                <h1>
+                    <ul>
+                        <li>
+                            <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg" alt="" />
+                            <span>Spider Man</span>
+                        </li>
+                        <li>
+                            <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg" alt="" />
+                            <span>Spider Man</span>
+                        </li>
+                        <li>
+                            <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg" alt="" />
+                            <span>Spider Man</span>
+                        </li>
+                    </ul>
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
