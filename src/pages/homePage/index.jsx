2004b337aaec2c9a102e737807eeca424719e88b
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-03-29 20:49:56 -0300

    test

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Container, MovieList, MovieListItem } from './styles';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { APIKey } from '../../config/key';
 
 function HomePage() {
 
     const [moviesArray, setMoviesArray] = useState([]);
+    const image_path = 'https://image.tmdb.org/t/p/w500';
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}`)
+            .then(response => response.json())
+            .then(data => setMoviesArray(data.results))
+    }, []);
 
     return (
         <>
@@ -16,7 +24,7 @@ function HomePage() {
                         {moviesArray.map((movie => {
                             return (
                                 <MovieListItem key={movie.id}>
-                                    <a href=""><img src={movie.image_url} alt={movie.title} /></a>
+                                    <a href=""><img src={`${image_path}${movie.poster_path}`} alt={movie.title} /></a>
                                     <span>{movie.title}</span>
                                 </MovieListItem>
                             )
