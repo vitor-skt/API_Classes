cf41c3aaa81560c3b17c3ee7b8edab418b82c4b0
Vitor Oliveira <vitor.oliveiramzx@hotmail.com>
2022-04-09 23:24:48 -0300

    Got error 404 on details

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { APIKey } from '../../config/key'
 
 function Details() {
 
     const id = useParams()
-    console.log(id)
+
+    const [movie, setMovie] = useState({})
+    const image_path = 'https://image.tmdb.org/t/p/w500';
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${APIKey}`)
+            .then(response => response.json())
+            .then(data => {
+
+                const movie = {
+                    id,
+                    title: data.title,
+                    overview: data.overview,
+                    image: `${image_path}${data.poster_path}`,
+                    releaseDate: data.release_date,
+                }
+                setMovie(movie)
+
+            })
+        console.log(movie.data)
+    }, [id])
 
     return (
         <>
